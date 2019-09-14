8134a07838ffce2e8ff7d324258e9c4abeeba219
AB <aarushban@gmail.com>
2019-09-14 16:03:02 -0400

    Using CSV to add labels for supervised learning

--- text_processing.py
+++ text_processing.py
@@ -2,32 +2,34 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-#regex matching
-import re
+import re #regex matching
+# import enchant #dictionary
 
-#import list of stopwords (conjunctions, prepositions)
+#import list of stopwords (conjunctions, prepositions) and dictionary words
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
+#for reading csv training data
+import pandas as pd 
 
 
-def process_data():
+def process_data(dataset):
     # open dataset with the URLs of existing policies
-    file_path = open('data_agreement_urls.txt', 'r')
+    # file_path = open('website_policy_data.csv', 'r')
+    data_frame = pd.read_csv(dataset)
 
-    for line in file_path.readlines():
+    for index,row in data_frame.iterrows():
         #read the webpage
-        response = urllib.request.urlopen(line)
+        response = urllib.request.urlopen(row['Links'])
         html = response.read()
 
         #take out words/tokens from webapge
         soup = BeautifulSoup(html, 'html5lib')
-        text = soup.get_text(strip=True)
+        text = soup.body.get_text(strip=True)
         tokens = [t for t in text.split()]
 
         sr = stopwords.words('english')
-
         clean_tokens = tokens[:] #ALL words on webpage
         important_words_only = tokens[:] #only key words (conjunctions and prepositions removed)
         
@@ -46,8 +48,13 @@ def process_data():
         print(clean_tokens) 
 
         #plotting 20 most frequent IMPORTANT words
-        freq = nltk.FreqDist(important_words_only)
-        freq.plot(20, cumulative=False)
+        # freq = nltk.FreqDist(important_words_only)
+        # freq.plot(20, cumulative=False)
+
+
+def assess_website(url):
+    response = urllib.request.urlopen(url)
+
 
 if __name__== "__main__":
-    process_data()
+    process_data('website_policy_data.csv')
