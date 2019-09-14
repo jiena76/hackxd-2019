511b8a0914c7692a395a4d44af526f3d76dc1e14
AB <aarushban@gmail.com>
2019-09-14 13:14:11 -0400

    Update text_processing.py
    Added code to extract words

---
+++ text_processing.py
@@ -0,0 +1,34 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import nltk
+nltk.download('stopwords') #get only stopwords we need
+from nltk.corpus import stopwords
+
+# open dataset with the URLs of existing policies
+file_path = open('data_agreement_urls.txt', 'r')
+
+
+count = 0
+for line in file_path.readlines():
+    count += 1
+    # print("line is ", line)
+    response = urllib.request.urlopen(line)
+    html = response.read()
+    # print(html)
+    soup = BeautifulSoup(html, 'html5lib')
+    text = soup.get_text(strip=True)
+    # print(text)
+    tokens = [t for t in text.split()]
+    print(tokens)
+
+    # sr = stopwords.words('english')
+    # clean_tokens = tokens[:]
+    # for token in tokens:
+    #     if token in stopwords.words('english'):
+
+    #         clean_tokens.remove(token)
+
+    # freq = nltk.FreqDist(clean_tokens)
+    # for key, val in freq.items():
+    #     print(str(key) + ':' + str(val))
+    # freq.plot(20, cumulative=False)
