bfff14a457cbd7fe83ec3d9ebe44c205aee10852
AB3000 <aarushban@gmail.com>
2019-09-19 16:35:20 -0400

    End-to-end testing working for email data

--- test_processing_temp.py
+++ test_processing_temp.py
@@ -30,9 +30,16 @@ link = 'https://www.facebook.com/legal/terms/update'
 def predict_collection(attribute_file, link):
     df = pd.read_csv(attribute_file)
 
+    attribute = attribute_file.split('_')[0]
+
+    #remove all stopwords from training data
+    stop = stopwords.words('english')
+    df['Sentences_Without_stopwords'] = df['Sentence'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+
     """get all sentences with label "1", meaning these sentences imply that 
     the website is COLLECTING the specified attribute"""
     data_collecting_attribute = df.loc[df['Label'] == 1, 'Sentence']
+    # print(data_collecting_attribute)
 
     """get all sentences with label "1", meaning these sentences imply that 
     the website is NOT COLLECTING the specified attribute"""
@@ -51,13 +58,42 @@ def predict_collection(attribute_file, link):
     train_model(tagged_collecting_data, 'collecting2v.model')
     train_model(tagged_not_collecting_data, 'not_collecting2v.model')
 
+    model_collecting = Doc2Vec.load('collecting2v.model')
+    model_not_collecting = Doc2Vec.load('not_collecting2v.model')
+
+    #to find the vector of a document which is not in training data
+    sentences = assess_website_for_attribute(link, attribute)
+
+    #keeps track of how many sentences match collecting data or not
+    collecting_count = not_collecting_count = 0 
+    for i in sentences:
+        test_data = word_tokenize(i.lower())
+        # print(test_data)
+        v_collect = model_collecting.infer_vector(test_data)
+        v_not_collect =  model_not_collecting.infer_vector(test_data)
+        # to find most similar doc using tags
+        collect_similarity = model_collecting.docvecs.most_similar(positive=[v_collect],topn=1)[0][1]
+        not_collect_similarity = model_not_collecting.docvecs.most_similar(positive=[v_not_collect],topn=1)[0][1]
+
+        print("collect cosine:",collect_similarity)
+        print("not collect cosine:",not_collect_similarity)
+        if collect_similarity >= not_collect_similarity:
+            collecting_count+=1
+        else:
+            not_collecting_count+=1
+
+    if  not_collecting_count > collecting_count:
+        return 'This website may collect ' + attribute + ', but it does not sell it to third-parties.'
+    else:
+        return 'This website gives ' + attribute + ' to third parties'
+    
 
 def train_model(tagged_data, model_name): 
     max_epochs = 100
     vec_size = 20
     alpha = 0.025
 
-    model = Doc2Vec(size=vec_size,
+    model = Doc2Vec(vector_size=vec_size,
                     alpha=alpha, 
                     min_alpha=0.00025,
                     min_count=1,
@@ -66,10 +102,11 @@ def train_model(tagged_data, model_name):
     model.build_vocab(tagged_data)
 
     for epoch in range(max_epochs):
-        print('iteration {0}'.format(epoch))
+        # print('iteration {0}'.format(epoch))
         model.train(tagged_data,
                     total_examples=model.corpus_count,
-                    epochs=model.iter)
+                    epochs=5)
+       
         # decrease the learning rate
         model.alpha -= 0.0002
         # fix the learning rate, no decay
@@ -78,17 +115,15 @@ def train_model(tagged_data, model_name):
     return model.save(model_name)
 
 
-
-
-
-
-def assess_website(link):
+def assess_website_for_attribute(link, attrib):
     """
         Parameters: 
             link: the URL to the policy
         Returns: 
             clean_tokens: All sentences on the website 
     """
+
+    #print('attrib is', attrib)
     # read the webpage
     response = urllib.request.urlopen(link)
     html = response.read()
@@ -97,20 +132,19 @@ def assess_website(link):
     soup = BeautifulSoup(html, 'html5lib')
     text = soup.body.get_text(strip=True)
     sentences = (sent_tokenize(text))
-    # print(sentences)
 
-    # clean_tokens = sentences[:] #ALL sentences on webpage
-
-    for words in sentences:
+    for words in sentences[:]:
         # lazy way of removing sentences with just html in webpage
-        if '{' in words or '}' in words or '[]' in words:
+        if '{' in words or '}' in words or "[]" in words:
             sentences.remove(words)
-
-    print(sentences)
+            continue
+        if attrib not in words:
+            sentences.remove(words)
+        
 
     return sentences
 
 
 if __name__ == "__main__":
     # assess_website(link)
-    predict_collection('email_data.csv', link)
+    print(predict_collection('email_data.csv', link))
