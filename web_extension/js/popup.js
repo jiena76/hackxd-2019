e14fcf459ae38da67e9952e54181e4cf2185c6f0
Jieun Lee <jieun@purdue.edu>
2019-09-21 11:15:28 -0400

    update localStorage[curr_url] when the tab updates

--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -1,16 +1,16 @@
 const server = "http://localhost:8000/";
 
 document.getElementById("button-url").onclick = function () {
-  // alert(localStorage["current_url"]);
   const body = {
     url: localStorage["current_url"],
   };
+  // alert(body.url);
   const parameters = {
     headers: {'Content-type': 'application/json'},
     body: JSON.stringify(body),
     method: "POST"
   };
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
-    console.log(res);
+    print(res.json["url"])
   }).catch(error=>console.log(error));
 }
