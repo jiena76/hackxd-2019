0f8280cd30e372d0b9e50c6a110806f543415371
Jieun Lee <jieun@purdue.edu>
2019-09-21 11:15:56 -0400

    remove stupid print statement under javascript file

--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -11,6 +11,6 @@ document.getElementById("button-url").onclick = function () {
     method: "POST"
   };
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
-    print(res.json["url"])
+    console.log(res.json["url"])
   }).catch(error=>console.log(error));
 }
