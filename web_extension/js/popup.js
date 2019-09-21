87507e53641ec3f0dc61d0014e3cc42daad4e14a
Huberrrr <hubermjonathan@gmail.com>
2019-09-21 12:25:00 -0400

    fixed loader

--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -5,26 +5,20 @@ document.getElementById("button-url").onclick = function () {
     url: localStorage["current_url"],
   };
 
-  console.log('BODY IS', body);
-  // alert(body.url);
   const parameters = {
     headers: {'Content-type': 'application/json'},
     body: JSON.stringify(body),
     method: "POST"
   };
+
+  document.getElementById("button-url").classList.add("hidden");
+  document.getElementById("loader").classList.remove("hidden");
+
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
-    document.getElementById("button-url").classList.add("hidden");
-    document.getElementById("loader").classList.remove("hidden");
-    setTimeout(() => {
-      document.getElementById("loader").classList.add("hidden");
-      document.getElementById("desc").classList.remove("hidden");
-    }, 3000);
+    document.getElementById("loader").classList.add("hidden");
+    document.getElementById("desc").classList.remove("hidden");
   }).catch((error) => {
-    document.getElementById("button-url").classList.add("hidden");
-    document.getElementById("loader").classList.remove("hidden");
-    setTimeout(() => {
-      document.getElementById("loader").classList.add("hidden");
-      document.getElementById("error").classList.remove("hidden");
-    }, 3000);
+    document.getElementById("loader").classList.add("hidden");
+    document.getElementById("error").classList.remove("hidden");
   });
 }
