e58128d0a75873c3353abcdb27ce9ad31b323bba
AB3000 <aarushban@gmail.com>
2019-09-21 12:00:50 -0400

    Update app.py
    Has the python code connected now

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, json, request
+from backend import text_processing
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def test_route():
+
   print(request.json["url"])
+  text_processing.train_model('data/name_data.csv',  request.json["url"])
+  text_processing.train_model('data/email_data.csv', request.json["url"])
   return json.dumps(request.json)
 
 if __name__ == '__main__':
