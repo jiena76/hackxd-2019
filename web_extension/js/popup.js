21c9f3e7352037966ee47ad44b90b691ad77e302
Jieun Lee <jieun@purdue.edu>
2019-09-21 10:30:55 -0400

    test with url

--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -4,7 +4,7 @@ document.getElementById("button-url").onclick = function () {
   // alert(localStorage["current_url"]);
   const parameters = {
     headers: {'Content-type': 'text/html'},
-    body: localStorage["current_url"],
+    body: {ur: localStorage["current_url"]},
     method: "POST"
   };
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
