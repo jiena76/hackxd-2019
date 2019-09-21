0ac03a1a9e6c362403bc663d42289257c4432733
Huberrrr <hubermjonathan@gmail.com>
2019-09-21 11:28:34 -0400

    added design for the frontend

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def test_route():
   # print(request.form["url"])
-  return request.form["url"]
+  return request.json
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8000, debug=True)
--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -12,5 +12,11 @@ document.getElementById("button-url").onclick = function () {
   };
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
     console.log(res);
+    document.getElementById("button-url").classList.add("hidden");
+    document.getElementById("loader").classList.remove("hidden");
+    setTimeout(() => {
+      document.getElementById("loader").classList.add("hidden");
+      document.getElementById("desc").classList.remove("hidden");
+    }, 3000);
   }).catch(error=>console.log(error));
 }
