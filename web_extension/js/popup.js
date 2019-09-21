7049bd4576126c90f40e434c07ddfa10bbe8ed80
Jieun Lee <jieun@purdue.edu>
2019-09-21 10:35:07 -0400

    add json body

--- web_extension/js/popup.js
+++ web_extension/js/popup.js
@@ -2,9 +2,12 @@ const server = "http://localhost:8000/";
 
 document.getElementById("button-url").onclick = function () {
   // alert(localStorage["current_url"]);
+  const body = {
+    url: localStorage["current_url"],
+  };
   const parameters = {
     headers: {'Content-type': 'text/html'},
-    body: {url: localStorage["current_url"]},
+    body: JSON.stringify(body),
     method: "POST"
   };
   fetch(server, parameters).then(data=>{return data.json()}).then(res=>{
